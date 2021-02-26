a897b2fa4e54bd8021a32f37a6190bcaa254fb71
Paul Rosero <paul.rosero23@gmail.com>
2021-02-26 10:49:20 -0500

    refactored code so its much dry than what i had originally

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,11 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = (name) => {
-  console.log(name)
-  if (name) {
-    return `One for ${name}, one for me.`
-  } else {
-    return "One for you, one for me."
-  }
+// This works perfectly but the advice I got so I can pass this, is to do what I'm going to do next. 
+// export const twoFer = (name) => {
+//   console.log(name)
+//   if (name) {
+//     return `One for ${name}, one for me.`
+//   } else {
+//     return "One for you, one for me."
+//   }
+// };
+
+export const twoFer = (name = "you" ) => {
+  return `One for ${name}, one for me.`
 };
