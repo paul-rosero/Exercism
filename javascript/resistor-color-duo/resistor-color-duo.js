45f983f449b16290a64e91cb7cfdad1c87787157
Paul Rosero <paul.rosero23@gmail.com>
2021-03-31 13:05:21 -0400

    add code to make the tests pass

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,7 +2,10 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+const colorData = [
+  "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
+]
+export const decodedValue = (colorsArr) => {
+  const answer = colorsArr.slice(0, 2).map(i => colorData.indexOf(i)).join("");
+  return parseInt(answer)
+}
