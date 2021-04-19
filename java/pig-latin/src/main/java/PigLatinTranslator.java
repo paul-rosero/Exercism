89716e22766d8f28994f38b08c9eccf9b78c1ff0
Paul Rosero <paul.rosero23@gmail.com>
2021-04-19 19:18:17 -0400

    add code to pass the tests

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,5 +1,5 @@
 public class PigLatinTranslator{
     public String translate(String phrase) {
-        System.out.println(phrase);
-    }
-}
+        return phrase.replaceAll("\\b([aeiou]+|xr|yt)([a-z]*)\\b", "$0ay").replaceAll("\\b([a-z]*qu|[^ aeiouy]+|[^ aeiou]+)(y?[a-z]+)(?<!ay)\\b", "$2$1ay");
+    };
+};
