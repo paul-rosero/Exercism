4ddd509842fc9b63ce7feb0d4d09f5ad51edd869
Paul Rosero <paul.rosero23@gmail.com>
2021-04-20 10:38:40 -0400

    add code to try to pass first test

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,10 +1,7 @@
-/*
+public class Anagram{
+    String word;
+    public Anagram(String word) {
+        this.word = word;
+    }
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+};
